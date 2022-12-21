6660e114a5ff15943e36cbe06cfb5bf25c1361ce
active-objects-admin <active-objects-admin@gmail.com>
2022-12-22 01:10:42 +0200

    created/updated

--- factory.js
+++ factory.js
@@ -1,12 +1,15 @@
-function factory() {
+function Factory() {
     this.getInstance = () => {
         return new ClassTest();
     }
-     this.getAnotherInstance = () => {
-        return 'second instance';
+    this.getMessage = () => {
+        return 'hello world from Factory';
     }
 }
 
 function ClassTest() {
+    this.getMessage = () => {
+        return 'hello world from ClassTest';
+    }
 }
 ClassTest.prototype.name = 'ClassTest';
